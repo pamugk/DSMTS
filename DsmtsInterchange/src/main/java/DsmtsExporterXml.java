2da5bb8b462368411253736ae882c9f578fca76c
pamugk <48994871+pamugk@users.noreply.github.com>
2019-04-25 16:02:53 +0500

    Interaction Update

--- DsmtsInterchange/src/main/java/DsmtsExporterXml.java
+++ DsmtsInterchange/src/main/java/DsmtsExporterXml.java
@@ -1,20 +1,226 @@
+import java.awt.*;
 import java.io.OutputStream;
 import java.io.Writer;
+import java.io.File;
+import java.util.Collection;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import com.thoughtworks.xstream.XStream;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 public class DsmtsExporterXml implements VisualModelExporter {
+    //Метод, возвращающий поддерживаемый экспортером формат
     public String getSupportedFormat() {
-        return null;
+        return "DSMTS";
     }
 
+    //Метод, экспортирующий модель model в файл по указанному пути filePath
     public void ExportModel(VisualModel model, String filePath) {
-
+        Document doc = CreateXmlDocument();
+        WriteModel(doc, model);
+        WriteXmlDocument(doc, new StreamResult(new File(filePath)));
     }
 
+    //Метод, экспортирующий модель model в переданный поток outputStream
     public void ExportModel(VisualModel model, OutputStream outputStream) {
-
+        Document doc = CreateXmlDocument();
+        WriteModel(doc, model);
+        WriteXmlDocument(doc, new StreamResult(outputStream));
     }
 
+    //Метод, записывающий модель model переданным  writer
     public void ExportModel(VisualModel model, Writer writer) {
+        Document doc = CreateXmlDocument();
+        WriteModel(doc, model);
+        WriteXmlDocument(doc, new StreamResult(writer));
+    }
 
+    //Метод, создающий объект, представляющий XML-документ в оперативной памяти
+    private static Document CreateXmlDocument(){
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        Document doc = null;
+        try {
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+            doc = docBuilder.newDocument();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+        return doc;
+    }
+
+    //Метод, записывающий переданный Xml-документ в переданный выходной поток
+    private static void WriteXmlDocument(Document doc, StreamResult stream) {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = null;
+        try {
+            transformer = transformerFactory.newTransformer();
+        } catch (TransformerConfigurationException e) {
+            e.printStackTrace();
+            return;
+        }
+        DOMSource source = new DOMSource(doc);
+        transformer.setOutputProperty(OutputKeys.INDENT,"yes");
+        try {
+            transformer.transform(source, stream);
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Метод, формирующий на основе объекта визуальной модели соответствующий Xml-элемент и включающий его
+    //в Xml-документ
+    private static void WriteModel(Document doc, VisualModel model)
+    {
+        Element rootElement = doc.createElement("VisualModel");
+        WritePolarHypergraph(doc, rootElement, model.getStructure(), "Structure");
+        WriteModelView(doc, rootElement, model.getView(), "ModelView");
+        doc.appendChild(rootElement);
+    }
+
+    //Метод, формирующий на основе объекта гиперграфа с полюсами соответствующий Xml-элемент и включающий его
+    //в Xml-документ
+    private static void WritePolarHypergraph(Document doc, Element root, PolarHypergraph hypergraph, String header)
+    {
+        Element hypergraphEl = doc.createElement(header);
+        root.appendChild(hypergraphEl);
+        if (hypergraph == null)
+            return;
+
+        Element nodesRoot = doc.createElement("Nodes");
+        hypergraph.getNodes().forEach(node -> WriteNode(doc, nodesRoot, node, "Node"));
+
+        Element polesRoot = doc.createElement("Poles");
+        hypergraph.getNodes().forEach(node -> WriteNode(doc, polesRoot, node, "Pole"));
+
+        Element linksRoot = doc.createElement("Hyperedges");
+        hypergraph.getNodes().forEach(node -> WriteNode(doc, linksRoot, node, "Hyperedge"));
+    }
+
+    //Метод, формирующий на основе объекта узла модели соответствующий Xml-элемент и включающий его
+    //в Xml-документ
+    private static void WriteNode(Document doc, Element root, Node node, String header)
+    {
+        Element nodeEl = doc.createElement(header);
+        nodeEl.setAttribute("ID", node.getId());
+        WritePolesHeadersXml(doc, nodeEl, node.getInputPoles(), "Input");
+        WritePolesHeadersXml(doc, nodeEl,node.getOutputPoles(), "Output");
+        WritePolarHypergraph(doc, nodeEl, node.getInnerStructure(), "InnerStructure");
+        root.appendChild(nodeEl);
+    }
+
+    //Метод, формирующий на основе объекта полюса соответствующий Xml-элемент и включающий его
+    //в Xml-документ
+    private static void WritePole(Document doc, Element root, Pole pole, String header)
+    {
+        Element poleEl = doc.createElement(header);
+        root.appendChild(poleEl);
+        if (pole == null)
+            return;
+        poleEl.setAttribute("ID", pole.getId());
+        poleEl.setAttribute("ParentID", pole.getParent().getId());
+    }
+
+    //Метод, формирующий на основе объекта гиперребра соответствующий Xml-элемент и включающий его
+    //в Xml-документ
+    private static void WriteHyperedge(Document doc, Element root, Hyperedge hyperedge)
+    {
+        Element hyperedgeEl = doc.createElement("Hyperedge");
+        hyperedgeEl.setAttribute("ID", hyperedge.getId());
+        WritePolesHeadersXml(doc, hyperedgeEl, hyperedge.getLinkedPoles(), "Linked");
+        WritePolarHypergraph(doc, hyperedgeEl, hyperedge.getInnerStructure(), "InnerStructure");
+    }
+
+    //Метод, формирующий на основе визуального представления модели соответствующий Xml-элемент и включающий его
+    //в Xml-документ
+    private static void WriteModelView(Document doc, Element root, ModelView modelView, String header)
+    {
+        Element mvEl = doc.createElement(header);
+        WriteViews(doc, mvEl, "NodeViews", "NodeView", modelView.NodeViews.values());
+        WriteViews(doc, mvEl, "PoleViews", "PoleView", modelView.PoleViews.values());
+        WriteViews(doc, mvEl, "LinkViews", "LinkView",modelView.LinkViews.values());
+        root.appendChild(mvEl);
+    }
+
+    //Метод, формирующий на основе представлений элементов соответствующий Xml-элемент и включающий его
+    //в Xml-документ
+    private static void WriteViews(Document doc, Element root, String header, String elHeader, Collection<View> views){
+        Element headerEl = doc.createElement(header);
+        headerEl.setAttribute("Count", String.valueOf(views.size()));
+        views.forEach(view -> WriteView(doc, headerEl, view, elHeader));
+        root.appendChild(headerEl);
+    }
+
+    //Метод, формирующий на основе объекта представления элемента модели
+    // соответствующий Xml-элемент и включающий его в Xml-документ
+    private static void WriteView(Document doc, Element root, View view, String header)
+    {
+        Element viewEl = doc.createElement(header);
+        root.appendChild(viewEl);
+        if (view == null)
+            return;
+        viewEl.setAttribute("AssociatedElementID", view.getAssociatedElementId());
+        WriteShape(doc, viewEl, view.getShape(), "Shape");
+        WriteStroke(doc, viewEl, view.getStroke(), "Stroke");
+        WriteRGBAColorXml(doc, viewEl, view.getBackColor(), "BackColor");
+        WriteRGBAColorXml(doc, viewEl, view.getStrokeColor(), "StrokeColor");
+        viewEl.appendChild(CreateTextNode(doc, "Name", view.getName()));
+        viewEl.appendChild(CreateTextNode(doc, "Content", view.getContent()));
+    }
+
+    //Метод, формирующий на основе объекта формы соответствующий Xml-элемент и включающий его
+    //в Xml-документ
+    private static void WriteShape(Document doc, Element root, Shape shape, String header){
+        Element shapeEl = doc.createElement(header);
+        root.appendChild(shapeEl);
+        if (shape == null)
+            return;
+        XStream stream = new XStream();
+        shapeEl.setAttribute("Value", stream.toXML(shape));
+    }
+
+    //Метод, формирующий на основе объекта черты соответствующий Xml-элемент и включающий его
+    //в Xml-документ
+    private static void WriteStroke(Document doc, Element root, Stroke stroke, String header){
+        Element strokeEl = doc.createElement(header);
+        root.appendChild(strokeEl);
+        if (stroke == null)
+            return;
+        XStream stream = new XStream();
+        strokeEl.setAttribute("Value", stream.toXML(stroke));
+    }
+
+    //Метод, формирующий текстовый элемент в XMl для переданных заголовка и значеия
+    private static Element CreateTextNode(Document doc, String header, String value){
+        Element el = doc.createElement(header);
+        el.appendChild(doc.createTextNode(value));
+        return el;
+    }
+
+    //Метод, формирующий на основе объекта цвета соответствующий Xml-элемент и включающий его
+    //в Xml-документ
+    private static void WritePolesHeadersXml(Document doc, Element root, Collection<Pole> set, String adj)
+    {
+        Element polesHeaderEl = doc.createElement(adj+"Poles");
+        polesHeaderEl.setAttribute(adj+"PoleCount", String.valueOf(set.size()));
+        set.forEach(pole -> polesHeaderEl.appendChild(CreateTextNode(doc, "PoleID", String.valueOf(pole.getId()))));
+        root.appendChild(polesHeaderEl);
+    }
+
+    //Метод, формирующий на основе объекта цвета соответствующий Xml-элемент и включающий его
+    //в Xml-документ
+    private static void WriteRGBAColorXml(Document doc, Element root, Color color, String start)
+    {
+        Element colorEl = doc.createElement(start);
+        colorEl.setAttribute("R", String.valueOf(color.getRed()));
+        colorEl.setAttribute("G", String.valueOf(color.getGreen()));
+        colorEl.setAttribute("B", String.valueOf(color.getBlue()));
+        colorEl.setAttribute("A", String.valueOf(color.getAlpha()));
+        root.appendChild(colorEl);
     }
 }
--- DsmtsInterchange/src/main/java/DsmtsImporterXml.java
+++ DsmtsInterchange/src/main/java/DsmtsImporterXml.java
@@ -1,20 +1,204 @@
+import java.awt.*;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
 
+import java.io.File;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import com.thoughtworks.xstream.XStream;
+import javafx.util.Pair;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 public class DsmtsImporterXml implements VisualModelImporter {
+    //Метод, возвращающий поддерживаемый импортером формат
     public String getSupportedFormat() {
-        return null;
+        return "DSMTS";
     }
 
+    //Метод, импортирующий из файла по указанному пути filePath
     public VisualModel ImportModel(String filePaths) {
-        return null;
+        Document doc;
+        try {
+            DocumentBuilder dBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            doc = dBuilder.parse(new File(filePaths));
+        } catch (ParserConfigurationException | IOException | SAXException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return ReadVisualModel(doc.getDocumentElement());
     }
 
+    //Метод, импортирующий модель из переданного потока inputStream
     public VisualModel ImportModel(InputStream inputStream) {
-        return null;
+        Document doc;
+        try {
+            DocumentBuilder dBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            doc = dBuilder.parse(inputStream);
+        } catch (ParserConfigurationException | IOException | SAXException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return ReadVisualModel(doc.getDocumentElement());
     }
 
+    //Метод, читающий модель из переданного reader
     public VisualModel ImportModel(VisualModel model, Reader reader) {
-        return null;
+        Document doc;
+        try {
+            DocumentBuilder dBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            doc = dBuilder.parse(reader.toString());
+        } catch (ParserConfigurationException | IOException | SAXException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return ReadVisualModel(doc.getDocumentElement());
+    }
+
+    //Метод, читающий из XML-элемента визуальную модельф
+    private static VisualModel ReadVisualModel(Element docEl)
+    {
+        VisualModel model = new VisualModel();
+        Element vmEl = (Element)docEl.getElementsByTagName("VisualModel").item(0);
+        model.setStructure(ReadHypergraph(vmEl));
+        model.setView(ReadModelView(vmEl));
+        return model;
+    }
+
+    //Метод, читающий из XML-элемента гиперграф
+    private static PolarHypergraph ReadHypergraph(Element node)
+    {
+        PolarHypergraph hypergraph = new PolarHypergraph();
+        HashMap<String, Pair<HashSet<String>, HashSet<String>>> nodesPoles = new HashMap<>();
+        ReadNodes((Element)node.getElementsByTagName("Nodes").item(0), hypergraph, nodesPoles);
+        ReadPoles((Element)node.getElementsByTagName("Poles").item(0), hypergraph, nodesPoles);
+        ReadLinks((Element)node.getElementsByTagName("Hyperedges").item(0), hypergraph);
+        return hypergraph;
+    }
+
+    //Метод, читающий из XML-элемента визуальное представление визуальной модели
+    private static ModelView ReadModelView(Element node){
+        ModelView mv = new ModelView();
+        mv.NodeViews = ReadViews((Element)node.getElementsByTagName("NodeViews").item(0), "NodeView");
+        mv.NodeViews = ReadViews((Element)node.getElementsByTagName("PoleViews").item(0), "PoleView");
+        mv.NodeViews = ReadViews((Element)node.getElementsByTagName("LinkViews").item(0), "LinkView");
+        return mv;
+    }
+
+    //Метод, читающий из XML-элемента визуальные представдения гиперграфа
+    private static HashMap<String, View> ReadViews(Element node, String singleViewName){
+        int N = Integer.parseInt(node.getAttribute("Count"));
+        NodeList viewList = node.getElementsByTagName(singleViewName);
+        return IntStream.range(0, N)
+                .mapToObj(i -> ReadView((Element) viewList.item(i)))
+                .collect(Collectors.toMap(View::getAssociatedElementId, view -> view, (a, b) -> b, HashMap::new));
+    }
+
+    //Метод, читающий из XML-элемента вершины гиперграфа
+    private static void ReadNodes(Element element, PolarHypergraph hypergraph,
+                                  HashMap<String, Pair<HashSet<String>, HashSet<String>>> nodesPoles)
+    {
+        int N = Integer.parseInt(element.getAttribute("NodeCount"));
+        NodeList nodeList = element.getElementsByTagName("Node");
+        IntStream.range(0, N).forEach(i -> {
+            Pair<HashSet<String>, HashSet<String>> poles = new Pair(new HashSet<String>(), new HashSet<String>());
+            Node node = ReadNode((Element) nodeList.item(i), poles);
+            nodesPoles.put(node.getId(), poles);
+            hypergraph.tryAddNode(node);
+        });
+    }
+
+    //Метод, читающий из XML-элемента полюса гиперграфа
+    private static void ReadPoles(Element polesEl, PolarHypergraph model,
+                                  HashMap<String, Pair<HashSet<String>, HashSet<String>>> nodesPoles)
+    {
+        int N = Integer.parseInt(polesEl.getAttribute("PoleCount"));
+        NodeList poles = polesEl.getElementsByTagName("Pole");
+        IntStream.range(0, N).mapToObj(i -> ReadPole((Element) poles.item(i), model)).forEach(pole -> {
+            if (nodesPoles.get(pole.getParent().getId()).getKey().contains(pole.getId()))
+                pole.getParent().tryAddInputPole(pole);
+            if (nodesPoles.get(pole.getParent().getId()).getValue().contains(pole.getId()))
+                pole.getParent().tryAddOutputPole(pole);
+        });
+    }
+
+    //Метод, читающий из XML-элемента гиперребра гиперграфа
+    private static void ReadLinks(Element linksEl, PolarHypergraph model)
+    {
+        int N = Integer.parseInt(linksEl.getAttribute("HyperedgeCount"));
+        NodeList hyperedges = linksEl.getElementsByTagName("Hyperedge");
+        IntStream.range(0, N).forEach(i -> model.tryAddHyperedge(ReadLink((Element) hyperedges.item(i), model)));
+    }
+
+    //Метод, читающий из XML-элемента вершину гиперграфа
+    private static Node ReadNode(Element nodeEl, Pair<HashSet<String>, HashSet<String>> poles)
+    {
+        Node node = new Node(nodeEl.getAttribute("ID"));
+        ReadPolesIDs((Element)nodeEl.getElementsByTagName("InputPoles").item(0), poles.getKey());
+        ReadPolesIDs((Element)nodeEl.getElementsByTagName("OutputPoles").item(0), poles.getValue());
+        node.setInnerStructure(ReadHypergraph((Element)nodeEl.getElementsByTagName("InnerStructure").item(0)));
+        return node;
+    }
+
+    //Метод, читающий из XML-элемента полюс
+    private static Pole ReadPole(Element poleEl, PolarHypergraph model)
+    {
+        String id = poleEl.getAttribute("ID");
+        String pID = poleEl.getAttribute("ParentID");
+        Node parent = model.tryGetNode(pID);
+        return new Pole(parent, id);
+    }
+
+    //Метод, читающий из XML-элемента гиперребро гиперграфа
+    private static Hyperedge ReadLink(Element hyperedgeEl, PolarHypergraph hypergraph)
+    {
+        Hyperedge hyperedge = new Hyperedge(hyperedgeEl.getAttribute("ID"));
+        ArrayList<String> linkedPoles = new ArrayList<>();
+        ReadPolesIDs((Element)hyperedgeEl.getElementsByTagName("LinkedPoles").item(0), linkedPoles);
+        linkedPoles.forEach(poleID -> hyperedge.tryAddPole(hypergraph.tryGetPole(poleID)));
+        hyperedge.setInnerStructure(ReadHypergraph((Element)hyperedgeEl.getElementsByTagName("InnerStructure").item(0)));
+        return hyperedge;
+    }
+
+    //Метод, преобразующий читающий из XML-элемента визуальное представление элемента
+    private static View ReadView(Element viewEl)
+    {
+        View view = new View();
+        view.setAssociatedElementID(viewEl.getAttribute("AssociatedElementID"));
+        XStream stream = new XStream();
+        view.setShape((Shape)stream.fromXML(((Element)viewEl.getElementsByTagName("Shape").item(0)).getAttribute("Value")));
+        view.setStroke((Stroke)stream.fromXML(((Element)viewEl.getElementsByTagName("Stroke").item(0)).getAttribute("Value")));
+        view.setBackColor(ReadRGBAColor((Element) viewEl.getElementsByTagName("BackColor").item(0)));
+        view.setStrokeColor(ReadRGBAColor((Element) viewEl.getElementsByTagName("StrokeColor").item(0)));
+        view.setName(viewEl.getElementsByTagName("Name").item(0).getNodeValue());
+        view.setContent(viewEl.getElementsByTagName("Content").item(0).getNodeValue());
+        return view;
+    }
+
+    //Метод, читающий из XML-элемента ID полюсов
+    private static void ReadPolesIDs(Element el, Collection<String> poleIDs)
+    {
+        int N = Integer.parseInt(el.getAttributes().item(0).getNodeValue());
+        NodeList ids = el.getElementsByTagName("PoleID");
+        for (int i = 0; i < N; i++)
+            poleIDs.add(ids.item(i).getNodeValue());
+    }
+
+    //Метод, читающий из XML-элемента цвет в формате RGBA
+    private static Color ReadRGBAColor(Element colorEl)
+    {
+        int R = Integer.parseInt(colorEl.getAttribute("R"));
+        int G = Integer.parseInt(colorEl.getAttribute("G"));
+        int B = Integer.parseInt(colorEl.getAttribute("B"));
+        int A = Integer.parseInt(colorEl.getAttribute("A"));
+        return new Color(R, G, B, A);
     }
 }
